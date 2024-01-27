0bc7a50f60b9d0d829b3da834f06d910a580c4cd
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-01-27 15:15:27 +0300

    2

--- src/components/NavB.vue
+++ src/components/NavB.vue
@@ -25,18 +25,45 @@
 
 <style scoped>
 .NavB {
+  width: 100%;
+  margin: auto;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
+  padding: 24px 0;
+  /* padding: 16px; */
+  /* padding: 16px 105px; */
+  background-color: aqua;
+  font-size: 16px;
 }
 ul {
   display: flex;
   list-style: none;
   justify-content: space-evenly;
   align-items: center;
-  gap: 30px;
+  gap: 39px;
 }
 li {
   display: flex;
+}
+.logo {
+  padding-left: 105px;
+}
+.nav-btns {
+  display: flex;
+  gap: 23px;
+}
+.nav-btns button {
+  background-color: white;
+  padding: 12px 22px;
+  font-size: 16px;
+  border-radius: 6px;
+  border: 1px solid #3b2e8c;
+  color: #3b2e8c;
+}
+.nav-btns button:nth-child(2) {
+  background-color: #3b2e8c;
+  color: white;
+  /* color: #ededed; */
 }
 </style>
