4d1eb678fa02232b48726503e97ef24709cfe05f
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-01-29 23:14:52 +0300

    27

--- src/components/AfterHeroSec.vue
+++ src/components/AfterHeroSec.vue
@@ -1,11 +1,40 @@
 <template>
   <div class="AfterHeroSec">
-    <div class="text-3xl font-bold underline text-red-400 w-full">
-      AfterHeroSec
+    <div class="w-full">
+      <div class="container flex w-[calc(100%-294px)] m-auto bg-slate-300">
+        <div class="bigBox flex flex-col gap-[45px]">
+          <div class="top-smallboxes flex gap-[45px] w-[100%]">
+            <div
+              class="w-[60px] h-[60px] text-[#6C56BD4A] rounded bg-[#674fc2]"
+            >
+              plue circl 1
+            </div>
+            <div class="w-[33%] text-[#6C56BD4A] rounded bg-[#B88FC6]">
+              prple circl 2
+            </div>
+            <div class="w-[33%] text-[#6C56BD4A] rounded bg-[#db665a]">
+              red circl 3
+            </div>
+          </div>
+          <div class="btm-smallboxes flex gap-[45px] w-full">
+            <div class="text-[#6C56BD4A] rounded bg-[#db665a]">red circl 4</div>
+            <div class="text-[#6C56BD4A] rounded bg-[#674fc2]">
+              plue circl 5
+            </div>
+            <div class="text-[#6C56BD4A] rounded bg-[#B88FC6]">
+              prpl circl 6
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+.bigBox div {
+  width: 33%;
+}
+</style>
