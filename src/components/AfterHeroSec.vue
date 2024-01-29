190700225bb4993e990f3115a441b3544ae20b8e
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-01-30 00:02:17 +0300

    29

--- src/components/AfterHeroSec.vue
+++ src/components/AfterHeroSec.vue
@@ -2,27 +2,97 @@
   <div class="AfterHeroSec">
     <div class="w-full">
       <div class="container flex w-[calc(100%-294px)] m-auto bg-slate-300">
-        <div class="bigBox flex flex-col gap-[45px]">
-          <div class="top-smallboxes flex gap-[45px] w-[100%]">
-            <div
-              class="w-[60px] h-[60px] text-[#6C56BD4A] rounded bg-[#674fc2]"
-            >
-              plue circl 1
+        <div class="bigBox flex flex-col w-full gap-[45px]">
+          <div class="top-smallboxes flex gap-[45px] w-full">
+            <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] bg-gray-100">
+              <div class="rounded-[50%] relative">
+                <img
+                  class="absolute left-0 top-0 w-[77px] h-[77px]"
+                  src="../assets/images/Ellipse 87.svg"
+                  alt="Ellipse 87 "
+                />
+                <img
+                  class="left-2 top-2 w-[60px] h-[60px]"
+                  src="../assets/images/Ellipse 72.svg"
+                  alt="Ellips  "
+                />
+              </div>
+              <div class="flex flex-col">
+                <img
+                  class="w-[24px] h-[24px]"
+                  src="../assets/images/translate.svg"
+                  alt="translate"
+                />
+                <p>
+                  Offered in multiple languages, serving the Arab region in
+                  particular
+                </p>
+              </div>
             </div>
-            <div class="w-[33%] text-[#6C56BD4A] rounded bg-[#B88FC6]">
-              prple circl 2
+            <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] bg-[#3d3b45]">
+              <div class="w-[60px] h-[60px] rounded-[50%] bg-[#843a7d]">
+                plue circl 2
+              </div>
+              <div class="flex flex-col">
+                <img
+                  class="w-[24px] h-[24px]"
+                  src="../assets/images/element-2.svg"
+                  alt="element-2.svg"
+                />
+                <p>Simple and advanced user interface</p>
+              </div>
             </div>
-            <div class="w-[33%] text-[#6C56BD4A] rounded bg-[#db665a]">
-              red circl 3
+            <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] bg-[#3d3b45]">
+              <div class="w-[60px] h-[60px] rounded-[50%] bg-[#cf3838]">3</div>
+              <div class="flex flex-col">
+                <img
+                  class="w-[24px] h-[24px]"
+                  src="../assets/images/data-2.svg"
+                  alt="data-2.svg"
+                />
+                <p>One Place for all tasks and data</p>
+              </div>
             </div>
           </div>
           <div class="btm-smallboxes flex gap-[45px] w-full">
-            <div class="text-[#6C56BD4A] rounded bg-[#db665a]">red circl 4</div>
-            <div class="text-[#6C56BD4A] rounded bg-[#674fc2]">
-              plue circl 5
+            <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] bg-[#3d3b45]">
+              <div class="w-[60px] h-[60px] rounded-[50%] bg-[#c24f4f]">
+                plue circl 4
+              </div>
+              <div class="flex flex-col">
+                <img
+                  class="w-[24px] h-[24px]"
+                  src="../assets/images/profile-2user.svg"
+                  alt="profile-2user.svg"
+                />
+                <p>Track your clients progress and updates</p>
+              </div>
             </div>
-            <div class="text-[#6C56BD4A] rounded bg-[#B88FC6]">
-              prpl circl 6
+            <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] bg-[#3d3b45]">
+              <div class="w-[60px] h-[60px] rounded-[50%] bg-[#af588a]">
+                plue circl 5
+              </div>
+              <div class="flex flex-col">
+                <img
+                  class="w-[24px] h-[24px]"
+                  src="../assets/images/task-square.svg"
+                  alt="task-square.svg"
+                />
+                <p>Manage your tasks with ease from any place</p>
+              </div>
+            </div>
+            <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] bg-[#3d3b45]">
+              <div class="w-[60px] h-[60px] rounded-[50%] bg-[#4f60c2]">
+                plue circl 6
+              </div>
+              <div class="flex flex-col">
+                <img
+                  class="w-[24px] h-[24px]"
+                  src="../assets/images/command.svg"
+                  alt="command.svg"
+                />
+                <p>Flexible & scalable according to the targeted sector</p>
+              </div>
             </div>
           </div>
         </div>
@@ -35,6 +105,6 @@
 
 <style scoped>
 .bigBox div {
-  width: 33%;
+  /* width: 33%; */
 }
 </style>
