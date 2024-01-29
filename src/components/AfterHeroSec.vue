d22f5b63cb3c19b913c95390be3fbb74058d3d60
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-01-30 00:38:20 +0300

    33

--- src/components/AfterHeroSec.vue
+++ src/components/AfterHeroSec.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="AfterHeroSec">
     <div class="w-full">
-      <div class="container flex w-[calc(100%-294px)] m-auto">
+      <div class="container flex w-[calc(100%-294px)] m-auto pt-[4rem]">
         <div class="bigBox flex flex-col w-full gap-[45px]">
           <div class="top-smallboxes flex gap-[45px] w-full">
             <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] p-2">
@@ -20,15 +20,13 @@
                   src="../assets/images/translate.svg"
                   alt="translate"
                 />
-                <p class="text-[#303030]">
+                <p class="text-[#303030] w-[77%]">
                   Offered in multiple languages, serving the Arab region in
                   particular
                 </p>
               </div>
             </div>
-            <div
-              class="flex gap-[8px] w-[100%] text-[#6C56BD4A] p-2 bg-gray-200"
-            >
+            <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] p-2">
               <div class="w-[20%] relative">
                 <span
                   class="rounded-[50%] absolute m-auto text-center w-[77px] h-[77px] bg-[#CCC5E3E3]"
@@ -44,12 +42,13 @@
                   src="../assets/images/element-2.svg"
                   alt="element-2.svg"
                 />
-                <p class="text-[#303030]">Simple and advanced user interface</p>
+                <p class="text-[#303030]">
+                  Simple and advanced <br />
+                  user interface
+                </p>
               </div>
             </div>
-            <div
-              class="flex gap-[8px] w-[100%] text-[#6C56BD4A] p-2 bg-gray-200"
-            >
+            <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] p-2">
               <div class="w-[20%] relative">
                 <span
                   class="rounded-[50%] absolute m-auto text-center w-[77px] h-[77px] bg-[#DB665A4A]"
@@ -65,14 +64,15 @@
                   src="../assets/images/data-2.svg"
                   alt="data-2.svg"
                 />
-                <p class="text-[#303030]">One Place for all tasks and data</p>
+                <p class="text-[#303030]">
+                  One Place for all tasks <br />
+                  and data
+                </p>
               </div>
             </div>
           </div>
           <div class="btm-smallboxes flex gap-[45px] w-full">
-            <div
-              class="flex gap-[8px] w-[100%] text-[#6C56BD4A] p-2 bg-gray-200"
-            >
+            <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] p-2">
               <div class="w-[20%] relative">
                 <span
                   class="rounded-[50%] absolute m-auto text-center w-[77px] h-[77px] bg-[#DB665A4A]"
@@ -88,12 +88,12 @@
                   src="../assets/images/profile-2user.svg"
                   alt="profile-2user.svg"
                 />
-                <p>Track your clients progress and updates</p>
+                <p class="text-[#303030] w-[77%]">
+                  Track your clients progress and updates
+                </p>
               </div>
             </div>
-            <div
-              class="flex gap-[8px] w-[100%] text-[#6C56BD4A] p-2 bg-gray-200"
-            >
+            <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] p-2">
               <div class="w-[20%] relative">
                 <span
                   class="rounded-[50%] absolute m-auto text-center w-[77px] h-[77px] bg-[#CCC5E3E3]"
@@ -109,12 +109,12 @@
                   src="../assets/images/task-square.svg"
                   alt="task-square.svg"
                 />
-                <p>Manage your tasks with ease from any place</p>
+                <p class="text-[#303030] w-[77%]">
+                  Manage your tasks with ease from any place
+                </p>
               </div>
             </div>
-            <div
-              class="flex gap-[8px] w-[100%] text-[#6C56BD4A] p-2 bg-gray-200"
-            >
+            <div class="flex gap-[8px] w-[100%] text-[#6C56BD4A] p-2">
               <div class="w-[20%] relative">
                 <span
                   class="rounded-[50%] absolute m-auto text-center w-[77px] h-[77px] bg-[#CCC5E3E3]"
@@ -130,7 +130,9 @@
                   src="../assets/images/command.svg"
                   alt="command.svg"
                 />
-                <p>Flexible & scalable according to the targeted sector</p>
+                <p class="text-[#303030] w-[77%]">
+                  Flexible & scalable according to the targeted sector
+                </p>
               </div>
             </div>
           </div>
