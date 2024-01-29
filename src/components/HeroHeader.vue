b949cb282ef810d8d58eeb7e8c3d2d15f61f6517
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-01-29 21:43:01 +0300

    21

--- src/components/HeroHeader.vue
+++ src/components/HeroHeader.vue
@@ -675,15 +675,13 @@ export default {};
 
 .svg-cricl-smal-left-7-7 {
   position: absolute;
-<<<<<<< HEAD
   left: 58px;
   /* top: 14px; */
   /* left: 48px; */
-=======
   /* left: 58px;
   top: 14px; */
   left: 48px;
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
+
   top: 12px;
   height: 23px;
   z-index: -1;
@@ -959,21 +957,12 @@ p {
     width: 100%;
     height: 100%;
     overflow: hidden;
-<<<<<<< HEAD
   }
   .HeroHeader {
     overflow: hidden;
     width: 100%;
     height: 100%;
     position: relative;
-=======
-
-  }
-  .HeroHeader {
-    padding-top: 75px;
-    overflow: hidden;
-
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
   }
 
   .circl-shdow-bg {
@@ -1005,7 +994,6 @@ p {
   h2,
   h3 {
     font-size: 2rem;
-    /* padding: 0.1rem 0 0.5rem 0; */
   }
   .svg-cricl-smal-right-4,
   .svg-cricl-smal-left-7,
@@ -1015,10 +1003,6 @@ p {
   .svg-cricl-smal-left-4 {
     display: none;
     overflow: hidden;
-<<<<<<< HEAD
-=======
-
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
   }
   .svg-cricl-smal-left-1 {
     width: 22px;
@@ -1031,10 +1015,6 @@ p {
     height: 30px;
     left: 12px;
     overflow: hidden;
-<<<<<<< HEAD
-=======
-
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
   }
   .svg-cricl-smal-right-2 {
     top: 400px;
@@ -1084,13 +1064,11 @@ p {
     font-size: 8px;
     /* background: turquoise; */
   }
-<<<<<<< HEAD
   .added-box-text-titles {
     width: 90%;
-=======
-  .added-box-text-titles{
-    width:90%;
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
+  }
+  .added-box-text-titles {
+    width: 90%;
   }
   .added-box-text-titles h4 {
     font-size: 10px;
--- src/components/NavB.vue
+++ src/components/NavB.vue
@@ -3,7 +3,6 @@
     <div class="logo">
       <a href="/"><img src="../assets/images/NavBar Logo.png" alt="logo" /></a>
     </div>
-<<<<<<< HEAD
 
     <div class="nav-list">
       <div v-if="isNavOpen" class="nav-links">
@@ -21,77 +20,46 @@
           <router-link to="/packages"><li>Packages</li></router-link>
         </ul>
       </div>
-=======
-    
 
-    <div class="nav-list">
+      <!-- ======= -->
 
-      <div v-if="isNavOpen" class="nav-links" >
-        
-      <ul>
-        <router-link to="/"  ><li>Home</li></router-link>
-        <router-link to="/features"  ><li>Features</li></router-link>
-        <router-link to= "/packages"  ><li>Packages</li></router-link>
-      </ul>
-      </div>
+      <div class="nav-list">
+        <div v-if="isNavOpen" class="nav-links">
+          <ul>
+            <router-link to="/"><li>Home</li></router-link>
+            <router-link to="/features"><li>Features</li></router-link>
+            <router-link to="/packages"><li>Packages</li></router-link>
+          </ul>
+        </div>
 
-      <div  v-else  class="nav-links-d-none-mob" >
-      <ul>
-        <router-link to="/"  ><li>Home</li></router-link>
-        <router-link to="/features"  ><li>Features</li></router-link>
-        <router-link to= "/packages"  ><li>Packages</li></router-link>
-      </ul>
-
-     
-    </div>
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
-      <!-- <ul v-else  >
+        <div v-else class="nav-links-d-none-mob">
+          <ul>
+            <router-link to="/"><li>Home</li></router-link>
+            <router-link to="/features"><li>Features</li></router-link>
+            <router-link to="/packages"><li>Packages</li></router-link>
+          </ul>
+        </div>
+        <!-- <ul v-else  >
         <router-link to="/" @click="closeNav" ><li>Home</li></router-link>
         <router-link to="/features" @click="closeNav" ><li>Features</li></router-link>
         <router-link to="/packages" @click="closeNav" ><li>Packages</li></router-link>
       </ul> -->
-<<<<<<< HEAD
-      <!-- Mobile layout: Show only in media -->
-=======
-                 <!-- Mobile layout: Show only in media -->
-   
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
+        <!-- Mobile layout: Show only in media -->
+        <!-- Mobile layout: Show only in media -->
 
-      <router-view />
-    </div>
-
-<<<<<<< HEAD
-    <!-- /////////////////////// -->
-
-    <svg
-      class="svg-menuBar-icon"
-      v-if="!isNavOpen"
-      @click="toggleNav"
-      viewBox="0 0 100 80"
-      width="40"
-      height="40"
-    >
-      <rect width="100" height="20"></rect>
-      <rect y="30" width="100" height="20"></rect>
-      <rect y="60" width="100" height="20"></rect>
-    </svg>
-
-    <!-- /////////////////////// -->
-
-    <div v-if="isNavOpen" class="nav-btns">
-      <button><a href="signIn">Sign In</a></button>
-      <button><a href="freetrial">Free Trial</a></button>
-    </div>
-
-    <div v-else class="nav-btns nav-btns-d-none-mob">
-      <!-- <div class="nav-btns" v-else  > -->
-      <button @click="closeNav"><a href="signIn">Sign In</a></button>
-      <button @click="closeNav"><a href="freetrial">Free Trial</a></button>
-=======
+        <router-view />
+      </div>
 
       <!-- /////////////////////// -->
 
-    <svg  class="svg-menuBar-icon" v-if="!isNavOpen" @click="toggleNav" viewBox="0 0 100 80" width="40" height="40"  >
+      <svg
+        class="svg-menuBar-icon"
+        v-if="!isNavOpen"
+        @click="toggleNav"
+        viewBox="0 0 100 80"
+        width="40"
+        height="40"
+      >
         <rect width="100" height="20"></rect>
         <rect y="30" width="100" height="20"></rect>
         <rect y="60" width="100" height="20"></rect>
@@ -99,93 +67,130 @@
 
       <!-- /////////////////////// -->
 
-      <div v-if="isNavOpen" class="nav-btns"   >
-        <button><a href="signIn"  >Sign In</a></button>
-        <button><a href="freetrial"  >Free Trial</a></button>
+      <div v-if="isNavOpen" class="nav-btns">
+        <button><a href="signIn">Sign In</a></button>
+        <button><a href="freetrial">Free Trial</a></button>
       </div>
 
+      <div v-else class="nav-btns nav-btns-d-none-mob">
+        <!-- <div class="nav-btns" v-else  > -->
+        <button @click="closeNav"><a href="signIn">Sign In</a></button>
+        <button @click="closeNav"><a href="freetrial">Free Trial</a></button>
 
-     <div v-else   class="nav-btns nav-btns-d-none-mob"   >
+        <!-- /////////////////////// -->
 
-    <!-- <div class="nav-btns" v-else  > -->
-      <button @click="closeNav" ><a href="signIn" >Sign In</a></button>
-      <button @click="closeNav" ><a href="freetrial">Free Trial</a></button>
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
-
-      <!-- svg start  -->
-      <div class="lang-icon">
         <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+          class="svg-menuBar-icon"
+          v-if="!isNavOpen"
+          @click="toggleNav"
+          viewBox="0 0 100 80"
+          width="40"
+          height="40"
         >
-          <g id="vuesax/linear/global">
-            <g id="global">
-              <path
-                id="Vector"
-                d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-                stroke="#3B2E8C"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                id="Vector_2"
-                d="M7.99998 3H8.99998C7.04998 8.84 7.04998 15.16 8.99998 21H7.99998"
-                stroke="#3B2E8C"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                id="Vector_3"
-                d="M15 3C16.95 8.84 16.95 15.16 15 21"
-                stroke="#3B2E8C"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                id="Vector_4"
-                d="M3 16V15C8.84 16.95 15.16 16.95 21 15V16"
-                stroke="#3B2E8C"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                id="Vector_5"
-                d="M3 8.99998C8.84 7.04998 15.16 7.04998 21 8.99998"
-                stroke="#3B2E8C"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </g>
-          </g>
+          <rect width="100" height="20"></rect>
+          <rect y="30" width="100" height="20"></rect>
+          <rect y="60" width="100" height="20"></rect>
+        </svg>
+
+        <!-- /////////////////////// -->
+
+        <div v-if="isNavOpen" class="nav-btns">
+          <button><a href="signIn">Sign In</a></button>
+          <button><a href="freetrial">Free Trial</a></button>
+        </div>
+
+        <div v-else class="nav-btns nav-btns-d-none-mob">
+          <!-- <div class="nav-btns" v-else  > -->
+          <button @click="closeNav"><a href="signIn">Sign In</a></button>
+          <button @click="closeNav"><a href="freetrial">Free Trial</a></button>
+
+          <!-- svg start  -->
+          <div class="lang-icon">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="vuesax/linear/global">
+                <g id="global">
+                  <path
+                    id="Vector"
+                    d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                    stroke="#3B2E8C"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    id="Vector_2"
+                    d="M7.99998 3H8.99998C7.04998 8.84 7.04998 15.16 8.99998 21H7.99998"
+                    stroke="#3B2E8C"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    id="Vector_3"
+                    d="M15 3C16.95 8.84 16.95 15.16 15 21"
+                    stroke="#3B2E8C"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    id="Vector_4"
+                    d="M3 16V15C8.84 16.95 15.16 16.95 21 15V16"
+                    stroke="#3B2E8C"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    id="Vector_5"
+                    d="M3 8.99998C8.84 7.04998 15.16 7.04998 21 8.99998"
+                    stroke="#3B2E8C"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </g>
+              </g>
+            </svg>
+          </div>
+
+          <!-- svg end  -->
+        </div>
+      </div>
+
+      <div class="naviconmeddiabox" v-if="isNavOpen" @click="closeNav">
+        <svg
+          class="svg-close-nav"
+          @click="closeNav"
+          viewPort="0 0 12 12"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          @click.stop
+        >
+          <line
+            x1="1"
+            y1="11"
+            x2="11"
+            y2="1"
+            stroke="#3b2e8c"
+            stroke-width="2"
+          />
+          <line
+            x1="1"
+            y1="1"
+            x2="11"
+            y2="11"
+            stroke="#3b2e8c"
+            stroke-width="2"
+          />
         </svg>
       </div>
-
-      <!-- svg end  -->
-      
-
-    </div>
-
-<<<<<<< HEAD
-    <div class="naviconmeddiabox" v-if="isNavOpen" @click="closeNav">
-      <svg
-        class="svg-close-nav"
-        @click="closeNav"
-        viewPort="0 0 12 12"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        @click.stop
-      >
-        <line x1="1" y1="11" x2="11" y2="1" stroke="#3b2e8c" stroke-width="2" />
-        <line x1="1" y1="1" x2="11" y2="11" stroke="#3b2e8c" stroke-width="2" />
-      </svg>
     </div>
   </div>
 </template>
@@ -194,69 +199,21 @@
 
 <script setup>
 import { ref } from "vue";
-=======
-      <div class="naviconmeddiabox" v-if="isNavOpen" @click="closeNav" >
-        <svg  class="svg-close-nav"   @click="closeNav"   
-          viewPort="0 0 12 12" version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          @click.stop
-          >
-          
-         <line x1="1" y1="11" 
-          x2="11" y2="1" 
-          stroke="#3b2e8c" 
-          stroke-width="2"/>
-          <line x1="1" y1="1" 
-          x2="11" y2="11" 
-          stroke="#3b2e8c" 
-          stroke-width="2"/>
-        </svg>
-      </div>
-
-
-  
-
-
-
-  </div>
-</template>
-
-
-<!-- //////////////////////////////////////// -->
-
-
-<script setup>
-
-
-import { ref } from 'vue';
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
 
 const isNavOpen = ref(false);
 
 const toggleNav = () => {
-<<<<<<< HEAD
   isNavOpen.value = !isNavOpen.value;
-=======
-   isNavOpen.value = !isNavOpen.value;
 
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
+  isNavOpen.value = !isNavOpen.value;
 };
 
 const closeNav = () => {
   isNavOpen.value = false;
 };
-<<<<<<< HEAD
 </script>
 
 <!-- //////////////////////////////////////// -->
-=======
-
-
-</script>
-
-<!-- //////////////////////////////////////// -->
-
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700&display=swap");
@@ -299,11 +256,7 @@ li:hover {
   font-family: "Cairo", sans-serif;
 }
 
-<<<<<<< HEAD
 .nav-list {
-=======
-.nav-list{
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
   overflow: hidden;
 }
 .nav-links {
@@ -344,26 +297,17 @@ li:hover {
   padding-left: 18px;
 }
 
-<<<<<<< HEAD
 .svg-menuBar-icon {
-=======
-.svg-menuBar-icon{
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
   fill: #3b2e8c;
   color: #3b2e8c;
   padding: 0.5rem;
   display: none;
   font-size: 4rem;
-<<<<<<< HEAD
+
   cursor: pointer;
 }
 
 .naviconmeddiabox {
-=======
-}
-
-.naviconmeddiabox{
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
   /* width: 10px;
   height: 10px; */
   background-color: #a695e2;
@@ -385,147 +329,123 @@ li:hover {
     padding: 0.8rem;
     gap: 0.8rem;
     overflow: hidden;
-<<<<<<< HEAD
   }
   .svg-menuBar-icon {
     display: flex;
   }
-=======
-
-  }
-.svg-menuBar-icon{
+}
+.svg-menuBar-icon {
   display: flex;
 }
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
-  .nav-btns {
-    gap: 0.2rem;
-  }
 
-  .NavB .logo a,
-  .NavB img {
-    width: 6rem;
-    font-size: 12px;
-  }
-  ul {
-    gap: 0.5rem;
-  }
-  .nav-btns button {
-    padding: 0.2rem 0.6rem;
-    font-size: 0.6rem;
-  }
-  .lang-icon {
-    width: 1.4rem;
-    gap: 0.2rem;
-    padding-left: 0.4rem;
-  }
-<<<<<<< HEAD
+.nav-btns {
+  gap: 0.2rem;
+}
 
-  /* ///////////////////////nav in media 1 //////////////////////// */
-
-  .naviconmeddiabox {
-=======
-  
+.NavB .logo a,
+.NavB img {
+  width: 6rem;
+  font-size: 12px;
+}
+ul {
+  gap: 0.5rem;
+}
+.nav-btns button {
+  padding: 0.2rem 0.6rem;
+  font-size: 0.6rem;
+}
+.lang-icon {
+  width: 1.4rem;
+  gap: 0.2rem;
+  padding-left: 0.4rem;
+}
 
 /* ///////////////////////nav in media 1 //////////////////////// */
 
-  .naviconmeddiabox{
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
-    display: flex;
-    width: 60%;
-    /* width: 25%; */
-    height: 25rem;
-    overflow: hidden;
-<<<<<<< HEAD
-  }
-  .svg-close-nav {
-    fill: #3b2e8c;
-    color: #3b2e8c;
-    padding: 1.8rem;
-  }
-  .svg-close-nav:checked,
-  .naviconmeddiabox:checked {
-    display: none;
-  }
-=======
-   }
-   .svg-close-nav{
-    fill: #3b2e8c;
-    color: #3b2e8c;
-    padding: 1.8rem ;
-   }
-   .svg-close-nav:checked , .naviconmeddiabox:checked{
-    display: none;
-   }
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
-  ul {
-    width: 25%;
-    flex-direction: column;
-    gap: 1rem;
-    text-align: left;
-    justify-content: flex-start;
-    align-items: flex-start;
-    position: absolute;
-    right: 25%;
-    top: 16%;
-    z-index: 2;
-    font-weight: 600;
-    font-size: 14px;
-    padding-left: 6%;
-<<<<<<< HEAD
-  }
-  li {
-    width: 100%;
-  }
-  .nav-btns button,
-  .nav-btns,
-  .lang-icon {
-=======
-    }
-    li{
-      width: 100%;
-    }
- .nav-btns button ,.nav-btns , .lang-icon {
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin: auto;
-    text-align: center;
-    width: 70%;
-    gap: 0.6rem;
-    font-size: 12px;
-    padding: 0.2rem;
-    overflow: hidden;
-<<<<<<< HEAD
-  }
-  .nav-btns {
-=======
+.naviconmeddiabox {
+  display: flex;
+  width: 60%;
+  /* width: 25%; */
+  height: 25rem;
+  overflow: hidden;
+}
+.svg-close-nav {
+  fill: #3b2e8c;
+  color: #3b2e8c;
+  padding: 1.8rem;
+}
+.svg-close-nav:checked,
+.naviconmeddiabox:checked {
+  display: none;
+}
 
-  }
-  .nav-btns{
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
-    position: absolute;
-    right: 25%;
-    top: 40%;
-    z-index: 2;
-    width: 25%;
-    overflow: hidden;
-<<<<<<< HEAD
-  }
+.svg-close-nav {
+  fill: #3b2e8c;
+  color: #3b2e8c;
+  padding: 1.8rem;
+}
+.svg-close-nav:checked,
+.naviconmeddiabox:checked {
+  display: none;
+}
+ul {
+  width: 25%;
+  flex-direction: column;
+  gap: 1rem;
+  text-align: left;
+  justify-content: flex-start;
+  align-items: flex-start;
+  position: absolute;
+  right: 25%;
+  top: 16%;
+  z-index: 2;
+  font-weight: 600;
+  font-size: 14px;
+  padding-left: 6%;
+}
+li {
+  width: 100%;
+}
+.nav-btns button,
+.nav-btns,
+.lang-icon {
+}
+li {
+  width: 100%;
+}
+.nav-btns button,
+.nav-btns,
+.lang-icon {
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  text-align: center;
+  width: 70%;
+  gap: 0.6rem;
+  font-size: 12px;
+  padding: 0.2rem;
+  overflow: hidden;
+}
+.nav-btns {
+}
+.nav-btns {
+  position: absolute;
+  right: 25%;
+  top: 40%;
+  z-index: 2;
+  width: 25%;
+  overflow: hidden;
+}
 
-  .nav-links-d-none-mob,
-  .nav-btns-d-none-mob {
-    display: none;
-  }
-=======
+.nav-links-d-none-mob,
+.nav-btns-d-none-mob {
+  display: none;
+}
 
-  }
-
-  .nav-links-d-none-mob , .nav-btns-d-none-mob{
-    display: none;
-  }
-   
->>>>>>> 14ff0d44364e9c2dbbc46cf50590c5004a0f8964
+.nav-links-d-none-mob,
+.nav-btns-d-none-mob {
+  display: none;
 }
 
 /* //////////////////////////////////////////// */
