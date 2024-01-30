37837d445dd70a042ec5fffda943a5541486b4cc
Sara Drwish <sooosoooadel555@gmail.com>
2024-01-30 13:41:21 +0200

    37 -wwkk

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div class="appClassGeneral">
     <NavB />
     <HeroHeader />
-    <AfterHeroSec />
-    <FeaturesSec />
+    <!-- <AfterHeroSec /> -->
+    <!-- <FeaturesSec /> -->
     <!-- <SignUp /> -->
   </div>
 </template>
@@ -11,16 +11,16 @@
 <script>
 import NavB from "./components/NavB.vue";
 import HeroHeader from "./components/HeroHeader.vue";
-import AfterHeroSec from "./components/AfterHeroSec.vue";
-import FeaturesSec from "./components/FeaturesSec.vue";
+// import AfterHeroSec from "./components/AfterHeroSec.vue";
+// import FeaturesSec from "./components/FeaturesSec.vue";
 // import SignUp from "./components/SignUp.vue";
 export default {
   name: "App",
   components: {
     NavB,
     HeroHeader,
-    AfterHeroSec,
-    FeaturesSec,
+    // AfterHeroSec,
+    // FeaturesSec,
     // SignUp,
   },
 };
