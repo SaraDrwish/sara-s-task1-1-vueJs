c69ac1a0a7d9a1c3ea78499629ef7bd0b1928a1c
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-01-31 00:56:03 +0300

    51

--- src/components/PackageSec.vue
+++ src/components/PackageSec.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Package">
-    <div class="container py-[2rem]">
+    <div class="container py-[2rem] mb-[120px]">
       <div class="flex flex-col md:gap-[75px] gap-8 p-1 bg-pink-500">
         <h2 class="text-[32px] font-[700] w-[90%] md:w-[40%]">
           Choose the appropriate package, and enjoy your Free Trial !
@@ -21,8 +21,8 @@
               <div class="flex gap-[10px] items-center mt-[13px] mb-[34px]">
                 <p class="text-[46px] text-white">25</p>
                 <div class="text-[16px] text-[#ffffffb8]">
-                  <span class="text-white"> SAR </span>
-                  <span class="text-white"> / Monthly </span>
+                  <span class=" "> SAR </span>
+                  <span class=" "> / Monthly </span>
                 </div>
               </div>
               <div class="flex">
@@ -37,8 +37,246 @@
             </div>
           </div>
           <div
-            class="flex flex-col gap-[4px] flex-1 rounded-[33px] px-[49px] py-[45px] bg-[#EDEDED]"
-          ></div>
+            class="flex flex-col gap-[4px] flex-1 text-[#303030] rounded-[33px] px-[49px] py-[45px] bg-[#EDEDED]"
+          >
+            <div class="">
+              <h2 class="text-[22px] mb-[41px]">Package Features</h2>
+            </div>
+            <ul
+              class="flex flex-col p-1 justify-between items-center w-[100%] gap-[21px] text-[18px] bg-green-700"
+            >
+              <li class="flex gap-[25%] justify-between items-center w-[100%]">
+                <div class="w-[100%]">Contact & Manage companies</div>
+                <div class="placeholder-purple-400">
+                  <svg
+                    class="w-[24px] h-[24px]"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:anim="http://www.w3.org/2000/anim"
+                    anim=""
+                    anim:transform-origin="50% 50%"
+                    anim:duration="1"
+                    anim:ease="ease-in-out"
+                  >
+                    <g id="vuesax/linear/tick-circle">
+                      <g id="vuesax/linear/tick-circle_2">
+                        <g id="tick-circle">
+                          <path
+                            id="Vector"
+                            d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                            fill="#674FC2"
+                          ></path>
+                          <path
+                            id="Vector_2"
+                            d="M7.75 12L10.58 14.83L16.25 9.17001"
+                            stroke="white"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+              </li>
+              <li
+                class="flex gap-[25%] justify-between items-center bg-yellow-300 w-[100%]"
+              >
+                <div class="">Manage Sales Pipeline</div>
+                <div class="">
+                  <svg
+                    class="w-[24px] h-[24px]"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:anim="http://www.w3.org/2000/anim"
+                    anim=""
+                    anim:transform-origin="50% 50%"
+                    anim:duration="1"
+                    anim:ease="ease-in-out"
+                  >
+                    <g id="vuesax/linear/tick-circle">
+                      <g id="vuesax/linear/tick-circle_2">
+                        <g id="tick-circle">
+                          <path
+                            id="Vector"
+                            d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                            fill="#674FC2"
+                          ></path>
+                          <path
+                            id="Vector_2"
+                            d="M7.75 12L10.58 14.83L16.25 9.17001"
+                            stroke="white"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+              </li>
+
+              <li
+                class="flex gap-[25%] justify-between items-center bg-yellow-300 w-[100%]"
+              >
+                <div class="">Manage Tasks</div>
+                <div class="">
+                  <svg
+                    class="w-[24px] h-[24px]"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:anim="http://www.w3.org/2000/anim"
+                    anim=""
+                    anim:transform-origin="50% 50%"
+                    anim:duration="1"
+                    anim:ease="ease-in-out"
+                  >
+                    <g id="vuesax/linear/tick-circle">
+                      <g id="vuesax/linear/tick-circle_2">
+                        <g id="tick-circle">
+                          <path
+                            id="Vector"
+                            d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                            fill="#674FC2"
+                          ></path>
+                          <path
+                            id="Vector_2"
+                            d="M7.75 12L10.58 14.83L16.25 9.17001"
+                            stroke="white"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+              </li>
+
+              <li
+                class="flex gap-[25%] justify-between items-center bg-yellow-300 w-[100%]"
+              >
+                <div class="">Track Sales Activities</div>
+                <div class="">
+                  <svg
+                    class="w-[24px] h-[24px]"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:anim="http://www.w3.org/2000/anim"
+                    anim=""
+                    anim:transform-origin="50% 50%"
+                    anim:duration="1"
+                    anim:ease="ease-in-out"
+                  >
+                    <g id="vuesax/linear/tick-circle">
+                      <g id="vuesax/linear/tick-circle_2">
+                        <g id="tick-circle">
+                          <path
+                            id="Vector"
+                            d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                            fill="#674FC2"
+                          ></path>
+                          <path
+                            id="Vector_2"
+                            d="M7.75 12L10.58 14.83L16.25 9.17001"
+                            stroke="white"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+              </li>
+
+              <li
+                class="flex gap-[25%] justify-between items-center bg-yellow-300 w-[100%]"
+              >
+                <div class="">Manage Projects</div>
+                <div class="">
+                  <svg
+                    class="w-[24px] h-[24px]"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:anim="http://www.w3.org/2000/anim"
+                    anim=""
+                    anim:transform-origin="50% 50%"
+                    anim:duration="1"
+                    anim:ease="ease-in-out"
+                  >
+                    <g id="vuesax/linear/tick-circle">
+                      <g id="vuesax/linear/tick-circle_2">
+                        <g id="tick-circle">
+                          <path
+                            id="Vector"
+                            d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                            fill="#674FC2"
+                          ></path>
+                          <path
+                            id="Vector_2"
+                            d="M7.75 12L10.58 14.83L16.25 9.17001"
+                            stroke="white"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+              </li>
+
+              <li
+                class="flex gap-[25%] justify-between items-center bg-yellow-300 w-[100%]"
+              >
+                <div class="">Analyze & Manage Reports</div>
+                <div class="">
+                  <svg
+                    class="w-[24px] h-[24px]"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:anim="http://www.w3.org/2000/anim"
+                    anim=""
+                    anim:transform-origin="50% 50%"
+                    anim:duration="1"
+                    anim:ease="ease-in-out"
+                  >
+                    <g id="vuesax/linear/tick-circle">
+                      <g id="vuesax/linear/tick-circle_2">
+                        <g id="tick-circle">
+                          <path
+                            id="Vector"
+                            d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                            fill="#674FC2"
+                          ></path>
+                          <path
+                            id="Vector_2"
+                            d="M7.75 12L10.58 14.83L16.25 9.17001"
+                            stroke="white"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
